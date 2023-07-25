7418d393a97aa38f1ea2f59ef6fab9d1c4615205
apoorv_121 <apoorvjha501@gmail.com>
2023-07-25 14:07:47 +0530

    greenCity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home/Home";
 import Payment from "./components/pages/Payment/Payment";
@@ -9,36 +9,41 @@ import Login from "./components/pages/Login/login.jsx";
 import PlatformApp from "./components/pages/Platform";
 import Activity from "./components/pages/Activity/Activity.jsx";
 import PersonalRecommend from "./components/pages/recommendation/PersonalRecommend";
-import Story1 from './components/pages/Story/Story1'
-import Story2 from './components/pages/Story/Story2'
+import Story1 from "./components/pages/Story/Story1";
+import Story2 from "./components/pages/Story/Story2";
 import Signup from "./components/pages/Signup/Signup";
 
 function App() {
   const isLoggedIn = localStorage.getItem("token") ? true : false;
-  console.log(localStorage.getItem("token"))
+  console.log(localStorage.getItem("token"));
   return (
     <div className="App">
-      <Routes>
-        <Route
-          path="/"
-          element={isLoggedIn ? <PlatformApp /> : <Navigate to="/signup" />}
-        >
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/plant" element={<Plant />} /> */}
-          {/* <Route path="/animal" element={<Animal />} /> */}
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/strayanimal" element={<StrayAnimal />} />
-          <Route path="/game" element={<Game />} />
-          <Route path="/organiseplantation" element={<OrganisePlantation />} />
-          <Route path="/activity" element={<Activity />} />
-          <Route path="/reccomend" element={<PersonalRecommend/>} />
-          <Route path="/gogreen" element={<Story1 />} />
-          <Route path="/helpanimal" element={<Story2 />} />
-        </Route>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={isLoggedIn ? <PlatformApp /> : <Navigate to="/signup" />}
+          >
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/plant" element={<Plant />} /> */}
+            {/* <Route path="/animal" element={<Animal />} /> */}
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/strayanimal" element={<StrayAnimal />} />
+            <Route path="/game" element={<Game />} />
+            <Route
+              path="/organiseplantation"
+              element={<OrganisePlantation />}
+            />
+            <Route path="/activity" element={<Activity />} />
+            <Route path="/reccomend" element={<PersonalRecommend />} />
+            <Route path="/gogreen" element={<Story1 />} />
+            <Route path="/helpanimal" element={<Story2 />} />
+          </Route>
 
-        <Route path="/auth" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+          <Route path="/auth" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
